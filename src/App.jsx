12729ae1b1b501031bd5b2019846532b07825c7e
Malinda <malindafiv56@gmail.com>
2024-08-14 16:21:11 +0530

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from "./components/Navbar"
 import HeroSection from './components/HeroSection'
+import About from './components/About'
 
 const App = () => {
   return (
     <main className='overflow-x-hidden bg-black tracking-tighter text-gray-200 antialiased'>
       <NavBar />
       <HeroSection />
+      <About />
     </main>
   )
 }
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -38,7 +38,7 @@ export const NAVIGATION_LINKS = [
 ];
 
 export const PROFILE = {
-  name: "Robert Butcher",
+  name: "Pasindu Malinda",
   info: "Full-stack developer with a flair for graphic design.",
 };
 
