4bb66474701412d7fb729bbf4365a50c0619d9a0
Malinda <malindafiv56@gmail.com>
2024-08-14 12:48:37 +0530

    full navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import NavBar from "./components/Navbar"
+import HeroSection from './components/HeroSection'
 
 const App = () => {
   return (
-    <main className='overflow-x-hidden bg-black'>
-
+    <main className='overflow-x-hidden bg-black tracking-tighter text-gray-200 antialiased'>
+      <NavBar />
+      <HeroSection />
     </main>
   )
 }
